1dab67483e1c5f2a92adacad3a84e38b1b4b554b
koh-sh <k@koh-sh.com>
2023-11-05 14:02:29 +0900

    test for gha

---
+++ internal/gha/gha_test.go
@@ -0,0 +1,69 @@
+package gha
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/google/go-github/v56/github"
+	"github.com/koh-sh/gha-timegraph/internal/types"
+)
+
+func Test_makeRun(t *testing.T) {
+	wftitle := "basic"
+	wftitle2 := "RunStarted is zero"
+	type args struct {
+		wfrun github.WorkflowRun
+	}
+	tests := []struct {
+		name string
+		args args
+		want types.Run
+	}{
+		{
+			name: "basic",
+			args: args{github.WorkflowRun{
+				Name: &wftitle,
+				CreatedAt: &github.Timestamp{
+					Time: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC),
+				},
+				UpdatedAt: &github.Timestamp{
+					Time: time.Date(2022, 4, 1, 9, 5, 0, 0, time.UTC),
+				},
+				RunStartedAt: &github.Timestamp{
+					Time: time.Date(2022, 4, 1, 9, 2, 0, 0, time.UTC),
+				},
+			}},
+			want: types.Run{
+				Name:      wftitle,
+				Starttime: time.Date(2022, 4, 1, 9, 2, 0, 0, time.UTC),
+				Elapsed:   180,
+			},
+		},
+		{
+			name: "RunStarted is zero",
+			args: args{github.WorkflowRun{
+				Name: &wftitle2,
+				CreatedAt: &github.Timestamp{
+					Time: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC),
+				},
+				UpdatedAt: &github.Timestamp{
+					Time: time.Date(2022, 4, 1, 9, 5, 0, 0, time.UTC),
+				},
+				RunStartedAt: &github.Timestamp{},
+			}},
+			want: types.Run{
+				Name:      wftitle2,
+				Starttime: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC),
+				Elapsed:   300,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := makeRun(tt.args.wfrun); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("makeRun() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
