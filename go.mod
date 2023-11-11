d49831073e76d0fc6e48d4ace1e9b37aa1dd79e6
koh-sh <k@koh-sh.com>
2023-11-11 16:52:28 +0900

    test: add test for gha.GetRuns

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.21.0
 
 require (
 	github.com/google/go-github/v56 v56.0.0
+	github.com/migueleliasweb/go-github-mock v0.0.22
 	github.com/schollz/progressbar/v3 v3.14.1
 	github.com/spf13/cobra v1.8.0
 	gonum.org/v1/plot v0.14.0
@@ -20,6 +21,7 @@ require (
 	github.com/go-pdf/fpdf v0.8.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -29,4 +31,5 @@ require (
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/term v0.14.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
--- internal/gha/gha_test.go
+++ internal/gha/gha_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/google/go-github/v56/github"
 	"github.com/koh-sh/gha-timegraph/internal/types"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
 )
 
 func Test_makeRun(t *testing.T) {
@@ -84,5 +85,239 @@ func TestRtnClient(t *testing.T) {
 				t.Errorf("RtnClient() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestGetRuns(t *testing.T) {
+	type args struct {
+		client   *github.Client
+		count    int
+		owner    string
+		repo     string
+		filename string
+		branch   string
+		status   string
+		silent   bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []types.Run
+		wantErr bool
+	}{
+		{
+			name: "basic",
+			args: args{
+				client:   mockClient("basic"),
+				count:    30,
+				owner:    "owner",
+				repo:     "repo",
+				filename: "test.yml",
+				branch:   "",
+				status:   "success",
+				silent:   false,
+			},
+			want: []types.Run{{
+				Name:      "test",
+				Starttime: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC),
+				Elapsed:   300,
+			}},
+			wantErr: false,
+		},
+		{
+			name: "old",
+			args: args{
+				client:   mockClient("old"),
+				count:    30,
+				owner:    "owner",
+				repo:     "repo",
+				filename: "test.yml",
+				branch:   "",
+				status:   "success",
+				silent:   false,
+			},
+			want: []types.Run{{
+				Name:      "test",
+				Starttime: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC),
+				Elapsed:   300,
+			}},
+			wantErr: false,
+		},
+		{
+			name: "pages",
+			args: args{
+				client:   mockClient("pages"),
+				count:    4,
+				owner:    "owner",
+				repo:     "repo",
+				filename: "test.yml",
+				branch:   "",
+				status:   "success",
+				silent:   true,
+			},
+			want: []types.Run{
+				{
+					Name:      "test",
+					Starttime: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC),
+					Elapsed:   300,
+				},
+				{
+					Name:      "test",
+					Starttime: time.Date(2022, 4, 1, 8, 0, 0, 0, time.UTC),
+					Elapsed:   300,
+				},
+				{
+					Name:      "test",
+					Starttime: time.Date(2022, 4, 1, 7, 0, 0, 0, time.UTC),
+					Elapsed:   300,
+				},
+				{
+					Name:      "test",
+					Starttime: time.Date(2022, 4, 1, 6, 0, 0, 0, time.UTC),
+					Elapsed:   300,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "empty",
+			args: args{
+				client:   mockClient("empty"),
+				count:    30,
+				owner:    "owner",
+				repo:     "repo",
+				filename: "test.yml",
+				branch:   "",
+				status:   "success",
+				silent:   true,
+			},
+			want:    []types.Run{},
+			wantErr: false,
+		},
+		{
+			name: "ratelimit",
+			args: args{
+				client:   mockClient("ratelimit"),
+				count:    1,
+				owner:    "owner",
+				repo:     "repo",
+				filename: "test.yml",
+				branch:   "",
+				status:   "success",
+				silent:   true,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetRuns(tt.args.client, tt.args.count, tt.args.owner, tt.args.repo, tt.args.filename, tt.args.branch, tt.args.status, tt.args.silent)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetRuns() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetRuns() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// return mock GitHub Client
+func mockClient(ptn string) *github.Client {
+	switch ptn {
+	case "ratelimit":
+		return github.NewClient(mock.NewMockedHTTPClient(
+			mock.WithRequestMatch(
+				mock.GetReposActionsWorkflowsRunsByOwnerByRepoByWorkflowId,
+				github.WorkflowRuns{
+					WorkflowRuns: []*github.WorkflowRun{},
+				},
+			),
+			mock.WithRateLimit(0, 0),
+		),
+		)
+
+	case "old":
+		return github.NewClient(mock.NewMockedHTTPClient(
+			mock.WithRequestMatch(
+				mock.GetReposActionsWorkflowsRunsByOwnerByRepoByWorkflowId,
+				github.WorkflowRuns{
+					WorkflowRuns: []*github.WorkflowRun{
+						{
+							Name:         github.String("test"),
+							UpdatedAt:    &github.Timestamp{Time: time.Date(2022, 4, 1, 9, 5, 0, 0, time.UTC)},
+							RunStartedAt: &github.Timestamp{Time: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC)},
+						},
+						{
+							Name:         github.String("test"),
+							UpdatedAt:    &github.Timestamp{Time: time.Date(2022, 4, 1, 9, 5, 0, 0, time.UTC)},
+							RunStartedAt: &github.Timestamp{Time: time.Date(2021, 4, 1, 9, 0, 0, 0, time.UTC)},
+						},
+					},
+				},
+			),
+		))
+
+	case "empty":
+		return github.NewClient(mock.NewMockedHTTPClient(
+			mock.WithRequestMatch(
+				mock.GetReposActionsWorkflowsRunsByOwnerByRepoByWorkflowId,
+				github.WorkflowRuns{
+					WorkflowRuns: []*github.WorkflowRun{},
+				},
+			),
+		))
+	case "pages":
+		return github.NewClient(mock.NewMockedHTTPClient(
+			mock.WithRequestMatchPages(
+				mock.GetReposActionsWorkflowsRunsByOwnerByRepoByWorkflowId,
+				github.WorkflowRuns{
+					WorkflowRuns: []*github.WorkflowRun{
+						{
+							Name:         github.String("test"),
+							UpdatedAt:    &github.Timestamp{Time: time.Date(2022, 4, 1, 9, 5, 0, 0, time.UTC)},
+							RunStartedAt: &github.Timestamp{Time: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC)},
+						},
+						{
+							Name:         github.String("test"),
+							UpdatedAt:    &github.Timestamp{Time: time.Date(2022, 4, 1, 8, 5, 0, 0, time.UTC)},
+							RunStartedAt: &github.Timestamp{Time: time.Date(2022, 4, 1, 8, 0, 0, 0, time.UTC)},
+						},
+					},
+				},
+				github.WorkflowRuns{
+					WorkflowRuns: []*github.WorkflowRun{
+						{
+							Name:         github.String("test"),
+							UpdatedAt:    &github.Timestamp{Time: time.Date(2022, 4, 1, 7, 5, 0, 0, time.UTC)},
+							RunStartedAt: &github.Timestamp{Time: time.Date(2022, 4, 1, 7, 0, 0, 0, time.UTC)},
+						},
+						{
+							Name:         github.String("test"),
+							UpdatedAt:    &github.Timestamp{Time: time.Date(2022, 4, 1, 6, 5, 0, 0, time.UTC)},
+							RunStartedAt: &github.Timestamp{Time: time.Date(2022, 4, 1, 6, 0, 0, 0, time.UTC)},
+						},
+					},
+				},
+			),
+		))
+
+	default:
+		return github.NewClient(mock.NewMockedHTTPClient(
+			mock.WithRequestMatch(
+				mock.GetReposActionsWorkflowsRunsByOwnerByRepoByWorkflowId,
+				github.WorkflowRuns{
+					WorkflowRuns: []*github.WorkflowRun{
+						{
+							Name:         github.String("test"),
+							UpdatedAt:    &github.Timestamp{Time: time.Date(2022, 4, 1, 9, 5, 0, 0, time.UTC)},
+							RunStartedAt: &github.Timestamp{Time: time.Date(2022, 4, 1, 9, 0, 0, 0, time.UTC)},
+						},
+					},
+				},
+			),
+		))
 	}
 }
